8f3238dc14639abc5abb634d39569bbc63e5d7cc
Briar <briar.j.mills@gmail.com>
2025-08-07 14:58:25 -0400

    Refactor to latest setup (#1)
    * Refactor to latest setup
    
    * Fix org check
    
    * automatic - Data bundle updated
    
    ---------
    
    Co-authored-by: b-j-mills <b-j-mills@users.noreply.github.com>

--- src/hdx/scraper/gdacs/__main__.py
+++ src/hdx/scraper/gdacs/__main__.py
@@ -9,57 +9,62 @@ import logging
 from os.path import dirname, expanduser, join
 
 from hdx.api.configuration import Configuration
+from hdx.data.user import User
 from hdx.facades.infer_arguments import facade
 from hdx.utilities.dateparse import parse_date
 from hdx.utilities.downloader import Download
 from hdx.utilities.loader import load_text
 from hdx.utilities.path import (
-    wheretostart_tempdir_batch,
+    script_dir_plus_file,
+    temp_dir_batch,
 )
 from hdx.utilities.retriever import Retrieve
 from hdx.utilities.saver import save_text
 
-from src.hdx.scraper.gdacs.gdacs import GDACS
+from hdx.scraper.gdacs._version import __version__
+from hdx.scraper.gdacs.pipeline import Pipeline
 
 logger = logging.getLogger(__name__)
 
-_USER_AGENT_LOOKUP = "hdx-scraper-gdacs"
+_LOOKUP = "hdx-scraper-gdacs"
 _SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
 _UPDATED_BY_SCRIPT = "HDX Scraper: GDACS"
 
 
 def main(
-    save: bool = True,
+    save: bool = False,
     use_saved: bool = False,
 ) -> None:
     """Generate datasets and create them in HDX
 
     Args:
-        save (bool): Save downloaded data. Defaults to True.
+        save (bool): Save downloaded data. Defaults to False.
         use_saved (bool): Use saved data. Defaults to False.
 
     Returns:
         None
     """
+    logger.info(f"##### {_LOOKUP} version {__version__} ####")
     configuration = Configuration.read()
+    User.check_current_user_write_access("gdacs")
     previous_build_date = parse_date(load_text("last_build_date.txt"))
-    with wheretostart_tempdir_batch(folder=_USER_AGENT_LOOKUP) as info:
-        temp_dir = info["folder"]
+
+    with temp_dir_batch(folder=_LOOKUP) as info:
+        tempdir = info["folder"]
         with Download() as downloader:
             retriever = Retrieve(
                 downloader=downloader,
-                fallback_dir=temp_dir,
+                fallback_dir=tempdir,
                 saved_dir=_SAVED_DATA_DIR,
-                temp_dir=temp_dir,
+                temp_dir=tempdir,
                 save=save,
                 use_saved=use_saved,
             )
-
-            gdacs = GDACS(configuration, retriever)
-            last_build_date, update = gdacs.parse_feed(previous_build_date)
+            pipeline = Pipeline(configuration, retriever)
+            last_build_date, update = pipeline.parse_feed(previous_build_date)
 
             if update:
-                dataset = gdacs.generate_dataset()
+                dataset = pipeline.generate_dataset()
                 dataset.update_from_yaml(
                     path=join(dirname(__file__), "config", "hdx_dataset_static.yaml")
                 )
@@ -78,8 +83,8 @@ if __name__ == "__main__":
     facade(
         main,
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
-        user_agent_lookup=_USER_AGENT_LOOKUP,
-        project_config_yaml=join(
-            dirname(__file__), "config", "project_configuration.yaml"
+        user_agent_lookup=_LOOKUP,
+        project_config_yaml=script_dir_plus_file(
+            join("config", "project_configuration.yaml"), main
         ),
     )
